cea50d73b72b611ed591af699b8953032e43c8bb
unknown <elampratorabdallah@yahoo.com>
2018-10-12 16:07:18 +0200

    Start Jquery Fixing

--- javascript/Components/grid-gutter.js
+++
@@ -1,4 +0,0 @@
-/*===== Grid Fix's =====*/
-$('.gutter-small').parent().addClass('wrap-gutter-s');
-$('.gutter-medium').parent().addClass('wrap-gutter-m');
-$('.gutter-large').parent().addClass('wrap-gutter-l');

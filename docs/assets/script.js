e4c018a9f046bb092f8865f95bf01f305c354aac
unknown <elampratorabdallah@yahoo.com>
2018-10-12 20:48:23 +0200

    Slick Doc P1

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -10,6 +10,40 @@ jQuery(function ($) {
         $(this).parent('li').siblings('li').removeClass('active').children('ul').slideUp();
     });
 
+    $('.slider-component').slick({
+        rtl: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 500,
+        dots: true,
+        arrows:true,
+        prevArrow: '<a href="javascript:void(0)" class="slick-prev ti-arrow-left-chevron"></a>',
+        nextArrow: '<a href="javascript:void(0)" class="slick-next ti-arrow-right-chevron"></a>',
+    });
+
+    $('.gallery-slider').slick({
+        rtl: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 500,
+        arrows:true,
+        prevArrow: '<a href="javascript:void(0)" class="slick-prev ti-arrow-left-chevron"></a>',
+        nextArrow: '<a href="javascript:void(0)" class="slick-next ti-arrow-right-chevron"></a>',
+        asNavFor:'.gallery-thumbnails'
+    });
+
+    $('.gallery-thumbnails').slick({
+        rtl: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 500,
+        arrows:false,
+        dots: true,
+        slidesToShow:4,
+        asNavFor:'.gallery-slider',
+        focusOnSelect:true,
+    });
+
     // Code Viewer
     new Clipboard('.copy-btn');
 
